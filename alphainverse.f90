eefd5c53f70ca6e931315dbe48c4c4e77c45fcd3
Ed Simmons <e.d.simmons@outlook.com>
2023-07-17 10:34:13 +0100

    Fix: formatting

--- alphainverse.f90
+++ alphainverse.f90
@@ -1,107 +1,107 @@
 SUBROUTINE alphainverse(PHIminus,PHI,PHIplus,Gminus,G,Gplus,Hminus,H,Hplus,delPolMode,F)
-USE inputdata,&
-ONLY:length,eta,sigma,xx,dp,epsilonn,shear,c,CURV,Init_gammaE, 	&
-     inv_tridiag_matrix,FlowShear,idelta_m,low_diag,diagonal,	&
-     upp_diag,super_diag, TaylorFlow
-IMPLICIT NONE
-INCLUDE 'mpif.h'
-REAL(KIND=dp),INTENT(IN)::delPolMode
-DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::PHIminus,PHI,PHIplus,Gminus,G,Gplus,Hminus,H,Hplus
-DOUBLE COMPLEX,DIMENSION(length),INTENT(OUT)::F
-DOUBLE COMPLEX,DIMENSION(length)::betaterm,gammaterm,deltaterm,couplingterm,DField,toinvert
-REAL(KIND=dp),PARAMETER::Delxx=ABS(xx(2)-xx(1))
-INTEGER::info, ipiv
+    USE inputdata,&
+        ONLY:length,eta,sigma,xx,dp,epsilonn,shear,c,CURV,Init_gammaE, 	&
+        inv_tridiag_matrix,FlowShear,idelta_m,low_diag,diagonal,	&
+        upp_diag,super_diag, TaylorFlow
+    IMPLICIT NONE
+    INCLUDE 'mpif.h'
+    REAL(KIND=dp),INTENT(IN)::delPolMode
+    DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::PHIminus,PHI,PHIplus,Gminus,G,Gplus,Hminus,H,Hplus
+    DOUBLE COMPLEX,DIMENSION(length),INTENT(OUT)::F
+    DOUBLE COMPLEX,DIMENSION(length)::betaterm,gammaterm,deltaterm,couplingterm,DField,toinvert
+    REAL(KIND=dp),PARAMETER::Delxx=ABS(xx(2)-xx(1))
+    INTEGER::info, ipiv
 
-COMPLEX*16,DIMENSION(length)::cen_diag
-COMPLEX*16,DIMENSION(length-1)::lft_diag,rgt_diag
+    COMPLEX*16,DIMENSION(length)::cen_diag
+    COMPLEX*16,DIMENSION(length-1)::lft_diag,rgt_diag
 
 ! DELTA-TERM
-  deltaterm =	O_delta(PHI) 				+ 	&
-		O_gamma(PHI) * FlowShear	 	+ 	&
-		O_beta(PHI)  * (FlowShear**2) 		+ 	&
-		O_alpha(PHI) * (FlowShear**3)
+    deltaterm =	O_delta(PHI) 				+ 	&
+        O_gamma(PHI) * FlowShear	 	+ 	&
+        O_beta(PHI)  * (FlowShear**2) 		+ 	&
+        O_alpha(PHI) * (FlowShear**3)
 
 
 ! GAMMA-TERM
-  gammaterm = 	O_gamma(G)				+	&
-		O_beta(G)  * 2.0_dp * FlowShear		+	&
-		O_alpha(G) * 3.0_dp * (FlowShear**2) 
+    gammaterm = 	O_gamma(G)				+	&
+        O_beta(G)  * 2.0_dp * FlowShear		+	&
+        O_alpha(G) * 3.0_dp * (FlowShear**2)
 
 
 ! BETA-TERM
-  betaterm =	O_beta(H)				+	&
-		O_alpha(H) * 3.0_dp * FlowShear
+    betaterm =	O_beta(H)				+	&
+        O_alpha(H) * 3.0_dp * FlowShear
 
 
 
 
 ! COUPLING TERM
-  couplingterm =	O_couple(Hminus,Hplus)					+	&
-			O_couple(Gminus,Gplus) * ( eta + 2.0_dp*FlowShear )	+	&
-			O_couple(PHIminus,PHIplus) * ( FlowShear**2 + eta*FlowShear)
+    couplingterm =	O_couple(Hminus,Hplus)					+	&
+        O_couple(Gminus,Gplus) * ( eta + 2.0_dp*FlowShear )	+	&
+        O_couple(PHIminus,PHIplus) * ( FlowShear**2 + eta*FlowShear)
 
 
 ! TO-INVERT
-  toinvert = -(gammaterm + betaterm + deltaterm) + CURV*epsilonn*couplingterm
+    toinvert = -(gammaterm + betaterm + deltaterm) + CURV*epsilonn*couplingterm
 
 
 
 ! INVERTED FIELD Fm
 ! the inverted tridiagonal matrix is calculated once at the beginning
-  toinvert(1)       =   DCMPLX( 0.0_dp,0.0_dp )
-  toinvert(length)  =   DCMPLX( 0.0_dp,0.0_dp )
-  !F = MATMUL(inv_tridiag_matrix,toinvert)
-  
-  lft_diag = low_diag
-  rgt_diag = upp_diag
-  cen_diag = diagonal
-  CALL ZGTSV( length,1,lft_diag,cen_diag,rgt_diag,toinvert,length,INFO  )
-  F = toinvert
+    toinvert(1)       =   DCMPLX( 0.0_dp,0.0_dp )
+    toinvert(length)  =   DCMPLX( 0.0_dp,0.0_dp )
+    !F = MATMUL(inv_tridiag_matrix,toinvert)
 
-  CONTAINS
+    lft_diag = low_diag
+    rgt_diag = upp_diag
+    cen_diag = diagonal
+    CALL ZGTSV( length,1,lft_diag,cen_diag,rgt_diag,toinvert,length,INFO  )
+    F = toinvert
+
+CONTAINS
 
 
 ! GAMMA Operator
-  FUNCTION O_gamma(field)
-  DOUBLE COMPLEX,DIMENSION(length) :: O_gamma
-  DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
-    O_gamma = (sigma**2) * ((xx-delPolMode)**2) * field
-  END FUNCTION O_gamma
+    FUNCTION O_gamma(field)
+        DOUBLE COMPLEX,DIMENSION(length) :: O_gamma
+        DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
+        O_gamma = (sigma**2) * ((xx-delPolMode)**2) * field
+    END FUNCTION O_gamma
 
 
 ! DELTA Operator
-  FUNCTION O_delta(field)
-  DOUBLE COMPLEX,DIMENSION(length) :: O_delta
-  DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
-    O_delta = (sigma**2) * ((xx-delPolMode)**2) * eta* field
-  END FUNCTION O_delta
+    FUNCTION O_delta(field)
+        DOUBLE COMPLEX,DIMENSION(length) :: O_delta
+        DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
+        O_delta = (sigma**2) * ((xx-delPolMode)**2) * eta* field
+    END FUNCTION O_delta
 
 
 ! ALPHA Operator
-  FUNCTION O_alpha(field)
-  DOUBLE COMPLEX,DIMENSION(length) :: O_alpha
-  DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
-    CALL Deriv( 2, field,DField,Delxx,length )
-    O_alpha = c*(shear**2)*DField - ( c+1.0_dp+idelta_m+TaylorFlow )*field
-  END FUNCTION O_alpha
+    FUNCTION O_alpha(field)
+        DOUBLE COMPLEX,DIMENSION(length) :: O_alpha
+        DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
+        CALL Deriv( 2, field,DField,Delxx,length )
+        O_alpha = c*(shear**2)*DField - ( c+1.0_dp+idelta_m+TaylorFlow )*field
+    END FUNCTION O_alpha
 
 
 ! BETA Operator
-  FUNCTION O_beta(field)
-  DOUBLE COMPLEX,DIMENSION(length) :: O_beta
-  DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
-    CALL Deriv( 2, field,DField,Delxx,length )
-    O_beta = eta*c*(shear**2)*DField + ( 1.0_dp - eta*(c+TaylorFlow) )*field
-  END FUNCTION O_beta
+    FUNCTION O_beta(field)
+        DOUBLE COMPLEX,DIMENSION(length) :: O_beta
+        DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field
+        CALL Deriv( 2, field,DField,Delxx,length )
+        O_beta = eta*c*(shear**2)*DField + ( 1.0_dp - eta*(c+TaylorFlow) )*field
+    END FUNCTION O_beta
 
 
 ! COUPLING Operator
-  FUNCTION O_couple(field_minus,field_plus)
-  DOUBLE COMPLEX,DIMENSION(length) :: O_couple
-  DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field_minus,field_plus
-    CALL Deriv( 1, field_plus-field_minus,DField,Delxx,length )
-    O_couple = ( field_plus+field_minus ) + shear*DField
-  END FUNCTION O_couple
+    FUNCTION O_couple(field_minus,field_plus)
+        DOUBLE COMPLEX,DIMENSION(length) :: O_couple
+        DOUBLE COMPLEX,DIMENSION(length),INTENT(IN)::field_minus,field_plus
+        CALL Deriv( 1, field_plus-field_minus,DField,Delxx,length )
+        O_couple = ( field_plus+field_minus ) + shear*DField
+    END FUNCTION O_couple
 
 
 
--- inputdata.f90
+++ inputdata.f90
@@ -1,107 +1,97 @@
 MODULE inputdata
-IMPLICIT NONE
+    IMPLICIT NONE
 
-SAVE
-INTEGER::					u
-INTEGER,PARAMETER::				initial=-800,final=800,							&	! Total grid points
-					   
-						length = (final-initial+1),						&
-						x0 = (length+1)/2,							&
-						dp = SELECTED_REAL_KIND(15,300),					&
+    SAVE
+    INTEGER:: u
+    INTEGER,PARAMETER :: initial=-800, final=800,              &    ! Total grid points
+        length = (final-initial+1),                             &    !
+        x0 = (length+1)/2,                                      &    !
+        dp = SELECTED_REAL_KIND(15,300),                        &    !
 
-						m0 = 70,								&
-						n0 = 50,								&
-						NumModes_by_2 = 40,							&	! Num. modes on either side of central m0
+        m0 = 70,                                                &    !
+        n0 = 50,                                                &    !
+        NumModes_by_2 = 40,                                     &    ! Num. modes on either side of central m0
 
-						InitialMode = m0-NumModes_by_2,						&
-						FinalMode   = m0+NumModes_by_2,						&
-						NumModes = FinalMode-InitialMode + 1
+        InitialMode = m0-NumModes_by_2,                         &    !
+        FinalMode   = m0+NumModes_by_2,                         &    !
+        NumModes = FinalMode-InitialMode + 1                         !
 
+    REAL(KIND=dp),PARAMETER :: MinRad = 0.5_dp,                &
+        MajRad = 5.0_dp,                                        &    ! Not used explicitly anywhere in the code
 
-REAL(KIND=dp),PARAMETER::			MinRad = 0.5_dp,							&	
-						MajRad = 5.0_dp,							& 	! Not used explicitly anywhere in the code
+        krhoi  =  0.2,                                          &    ! Parameters explained in the paper
+        shear  =  25.0_dp,                                      &    ! Bokshi PPCF 2016
+        CURV   =  1.0_dp,                                       &
+        etag   =  %ETA_G%_dp,                                   &    ! [2,5]
+        qedge  =  3.45_dp,                                      &    !
 
-						krhoi  =  0.2,								&	! Parameters explained in the paper
-						shear  =  25.0_dp,							&	! Bokshi PPCF 2016
-						CURV   =  1.0_dp,							&
-						etag   =  2.0_dp,							&	! [2,5]
-						qedge  =  3.45_dp,							&
+        etac  =  1062.5_dp,                                     &    !
+        tau   =  1.0_dp,                                        &    !
+        epsilonn  = %EPSILON_N%_dp,                             &    ! [0.01,0.02,0.03,0.04,0.08]
+        delta_m   = 0.0_dp,                                     &    ! I don't think this is used anywhere...
 
-						etac  =  1062.5_dp,							&
-						tau   =  1.0_dp,							&
-						epsilonn  = 0.01_dp,							&	! [0.01,0.02,0.03,0.04,0.08]
-						delta_m   = 0.0_dp,							&	! I don't think this is used anywhere...
+        b = krhoi**2,                                           &    !
+        c = b * tau,                                            &    !
 
-						b = krhoi**2,								&
-						c = b * tau,								&
-				
-						rs = MinRad * ( DBLE(m0)/(qedge*DBLE(n0)) )**(1/shear),			&
-						ktheta = DBLE(m0) / rs,							&
-						nqp = ktheta * shear,							&
+        rs = MinRad * ( DBLE(m0)/(qedge*DBLE(n0)) )**(1/shear), &    !
+        ktheta = DBLE(m0) / rs,                                 &    !
+        nqp = ktheta * shear,                                   &    !
 
-						dt = 0.1_dp,								&	! Time-step for RK4
-						RunTime = 2000.1_dp,							&
-						gammatol = 1e-4,							&
-						GenIso_Transition = 0.0_dp,						&	! Useful for transition studies, eg. Fig 7 in paper
-						Init_gammaE = 0.0e-3_dp,						&
-						Final_gammaE =0.0e-3_dp,						&
-						Quad_gammaE  = 0.0e-4_dp							! Effectively k1/n term in Fig. 6
+        dt = 0.1_dp,                                            &    ! Time-step for RK4
+        RunTime = 2000.1_dp,                                    &    !
+        gammatol = 0.0_dp,                                      &    ! Code will exit if the average change in gammae is below this value
+        GenIso_Transition = 0.0_dp,                             &    ! Useful for transition studies, eg. Fig 7 in paper
+        Init_gammaE = 0.0e-3_dp,                                &    !
+        Final_gammaE = 0.0e-3_dp,                               &    !
+        Quad_gammaE = 0.0e-4_dp                                      ! Effectively k1/n term in Fig. 6
 
-INTEGER,PARAMETER::				NumSteps = INT(RunTime/dt),						&
-						PtDensity = 10,								&	! Determines domain in radial coordinate xx
-						DelPrint  = 1000,							&	! How frequently to output to file
-						NumTheta = 360,								&	! Points in poloidal direction
-						navg = 100
-					
-REAL(KIND=dp)::					FlowOnOff = 1.0_dp,							&
-						ShearingRate,								&
-						dGammaE_dt = 1.0e-1_dp								! If dynamically adjusting flow-shear, uncomment
-																! line 134 onwards in itg.f90 and make appropriate 																	! minor changes to the file
-REAL(KIND=dp),DIMENSION(length),PARAMETER::	xx = (/ (u,u=initial,final,1) /) / DBLE(PtDensity)
+    INTEGER,PARAMETER :: NumSteps = INT(RunTime/dt),           &
+        PtDensity = 10,                                         &    ! Determines domain in radial coordinate xx
+        DelPrint  = 1000,                                       &    ! How frequently to output to file
+        NumTheta = 360,                                         &    ! Points in poloidal direction
+        navg = 100
 
+    REAL(KIND=dp) :: FlowOnOff = 1.0_dp,                       &    !
+        ShearingRate,                                           &    !
+        dGammaE_dt = 1.0e-1_dp
+    ! If dynamically adjusting flow-shear, uncomment
+    ! line 134 onwards in itg.f90 and make appropriate
+    ! minor changes to the file
 
-REAL(KIND=dp),DIMENSION(length),PARAMETER::	qprofile = DBLE(m0)/DBLE(n0),						&
- 						!qprofile = qedge * ( (xx/nqp + rs)/MinRad )**shear,			&	! Can give a q-profile
-						!eta = etag * ( 1.0_dp - etac * (xx/nqp)**2 ),				&	! can give a drive-profile
-						sigma = epsilonn / ( SQRT(b*tau)*qprofile ),				&
-						TaylorFlow = 0.0_dp
+    REAL(KIND=dp),DIMENSION(length),PARAMETER :: xx = (/ (u,u=initial,final,1) /) / DBLE(PtDensity)
 
-REAL(KIND=dp),DIMENSION(length)::		FlowShear = (Init_gammaE*xx) + (Quad_gammaE*(xx**2)),			&
-						eta										! Defined in itg.f90 - as are other parameters
-																! not defined here
-DOUBLE COMPLEX,PARAMETER::			ci = DCMPLX( 0.0_dp,1.0_dp ),						&
-						idelta_m = DCMPLX( 0.0_dp,delta_m )
+    REAL(KIND=dp),DIMENSION(length),PARAMETER :: qprofile = DBLE(m0)/DBLE(n0), &
+    !qprofile = qedge * ( (xx/nqp + rs)/MinRad )**shear,        &    ! Can give a q-profile
+    !eta = etag * ( 1.0_dp - etac * (xx/nqp)**2 ),              &    ! can give a drive-profile
+        sigma = epsilonn / ( SQRT(b*tau)*qprofile ),            &    !
+        TaylorFlow = 0.0_dp                                          !
 
-INTEGER::					MyRank,MySize,ierror,ModesPerProc
-DOUBLE COMPLEX,DIMENSION(length,length)::	inv_tridiag_matrix
+    REAL(KIND=dp),DIMENSION(length) :: FlowShear = (Init_gammaE*xx) + (Quad_gammaE*(xx**2)), &
+        eta   ! Defined in itg.f90 - as are other parameters not defined here
 
-COMPLEX*16,DIMENSION(length) ::			diagonal
-COMPLEX*16,DIMENSION(length-1)::		upp_diag,low_diag,super_diag
-COMPLEX*16,DIMENSION(length,NumModes)::		FULLtoinvert
+    DOUBLE COMPLEX,PARAMETER :: ci = DCMPLX( 0.0_dp,1.0_dp ), &     !
+        idelta_m = DCMPLX( 0.0_dp,delta_m )                          !
 
+    INTEGER :: MyRank, MySize, ierror, ModesPerProc                 ! MPI Variables
+    DOUBLE COMPLEX,DIMENSION(length,length) :: inv_tridiag_matrix
 
-LOGICAL,PARAMETER::				restart  = .FALSE.,							&	! If restarting simulation, define location of 
-						with_MPI = .TRUE.,                                                      &! FinalFields.txt from previous run
-                                                noiseStart = .false.,							&
-						calcThetaMaxima = .false.
+    COMPLEX*16,DIMENSION(length) :: diagonal                        !
+    COMPLEX*16,DIMENSION(length-1) :: upp_diag,low_diag,super_diag  !
+    COMPLEX*16,DIMENSION(length,NumModes) :: FULLtoinvert           !
 
-CHARACTER(20)::					runpath = 'run_3'
+    LOGICAL,PARAMETER :: restart = .FALSE.,                    &    ! If restarting simulation, define location of
+        with_MPI = .TRUE.,                                      &    ! FinalFields.txt from previous run
+        noiseStart = .false.,                                   &    !
+        calcThetaMaxima = .false.                                    !
+
+    CHARACTER(20) :: runpath = '%RUN_PATH%'                         ! The name of the path for file I/O.
 
 END MODULE inputdata
-
 
 !------!
 ! NOTE !
 !------!
 
-! (1) Running 'make' should compile and run the code. Check 'makefile' for the softwares needed. 
+! (1) Running 'make' should compile and run the code. Check 'makefile' for the softwares needed.
 !     f90 scripts need MPI, LAPACK and BLAS
 ! (2) All python scripts are for post-processing. Analysis scripts can be run before simulation has finished.
-
-
-
-
-
-
-
-
